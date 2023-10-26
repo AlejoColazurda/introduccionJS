9ce6356c56e16977c1433f95cf141b2e95b0fcfc
AlejoColazurda <acolazurda@gmail.com>
2023-10-26 17:16:38 -0300

    conociendo los strings  en js.

---
+++ JS/5-strings.js
@@ -0,0 +1,10 @@
+//String
+const producto = "monitor de 20 pulgadas";
+const producto2 = "monitor de 30 pulgadas";
+const producto3 = new String('Monitor de 50 pulgadas');
+
+
+
+console.log(producto);
+console.log(producto2);
+console.log(producto3);
