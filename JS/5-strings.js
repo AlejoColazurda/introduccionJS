e05dff3f96d01c59cffe3d381c5e0a19c4dcd4d7
AlejoColazurda <acolazurda@gmail.com>
2023-10-26 17:30:04 -0300

    Metodos para los strings

--- JS/5-strings.js
+++ JS/5-strings.js
@@ -1,10 +1,20 @@
 //String
-const producto = "monitor de 20 pulgadas";
-const producto2 = "monitor de 30 pulgadas";
-const producto3 = new String('Monitor de 50 pulgadas');
+const Tweet = "Aprendiendo Javascript con el curso de Desarrollo Web Completo";
+const producto1 = "Monitor de 20 pulgadas";
+// const producto2 = "monitor de 30 pulgadas";
+// const producto3 = new String('Monitor de 50 pulgadas');
 
 
+//Lenght es para conocer la extension del string.
+console.log(Tweet.length);
+console.log(producto1);
+// console.log(producto2);
+// console.log(producto3);
 
-console.log(producto);
-console.log(producto2);
-console.log(producto3);
+//indexOf es para encontrar un elemento en un string.
+console.log(Tweet.indexOf('Javascript'));
+console.log(producto1.indexOf('monitor'));
+
+//includes retorna true o flase
+console.log(Tweet.includes('Javascript'));
+console.log(producto1.includes('monitor'));
