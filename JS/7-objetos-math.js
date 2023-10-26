100918ce12e0697121b04c257ab178888635d666
AlejoColazurda <acolazurda@gmail.com>
2023-10-26 17:49:12 -0300

    Objeto Math en JS

---
+++ JS/7-objetos-math.js
@@ -0,0 +1,18 @@
+//Objeto Math
+
+let resultado;
+
+resultado = Math.PI;
+resultado = Math.round(2.2);
+resultado = Math.ceil(2.3);//ceil siempre redondea hacia arriba
+resultado = Math.floor(2.1);//ceil siempre redondea hacia abajo
+resultado = Math.sqrt(144);
+resultado = Math.abs(-200);
+resultado = Math.min(3, 5, 1, 8, 10, 7, 6);
+resultado = Math.max(2.1, 3, 6, 7, 89, 12);
+resultado = Math.random();
+resultado = Math.floor( Math.random() * 30);
+
+
+console.log(resultado);
+
