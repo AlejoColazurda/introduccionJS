3494fc2181a6f9844e7a61f9f45e7c4c60efdb3f
AlejoColazurda <acolazurda@gmail.com>
2023-10-26 17:59:54 -0300

    orden de las operaciones  en JS

---
+++ JS/8-orden-de-las-operaciones.js
@@ -0,0 +1,16 @@
+//ORDEN DE LAS OPERACIONES
+
+let resultado;
+
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * .2;
+
+console.log(resultado);
+
+//Incrementos
+let puntaje = 10;
+puntaje++;//11
+++puntaje;//12
+
+console.log(puntaje);
