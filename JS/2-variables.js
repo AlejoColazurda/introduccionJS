1b21b9d4a9f8a363e05273c5c5e2566290712158
AlejoColazurda <acolazurda@gmail.com>
2023-10-26 16:59:48 -0300

    variables en js.

---
+++ JS/2-variables.js
@@ -0,0 +1,23 @@
+//VARIABLES
+
+var producto ='Auriculares gamer'; //iniciar variable y darle valor.
+
+var disponible; //iniciar variable sin valor.
+
+producto = true; //reasignando el valor de la variable a tipo booleana.
+
+disponible = true; //reasignando el valor de la variable a tipo booleana.
+
+
+var producto1 = 'computadora',
+    disponible1 = true,
+    categoria = 'computadoras';
+
+
+//Estilos para declarar las variables.
+var nombre_producto = 'monitor HD'; //underscore.
+var nombreProducto = 'monitor HD'; //Camelcase.
+var nombreProducto = 'monitor HD'; //Pascal Case.
+var nombreproducto = 'monitor HD'; //lower case cero uso en js.
+
+console.log(producto);
