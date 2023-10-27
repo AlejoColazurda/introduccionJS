5933687837677cd52c78958b8c6d1b83ef686180
AlejoColazurda <acolazurda@gmail.com>
2023-10-27 14:09:16 -0300

    Spread Operator JS

---
+++ JS/14-spread-operator.js
@@ -0,0 +1,16 @@
+//OBJETOS
+const producto = {
+    nombreProducto: "Monitor 20 pulgadas",
+    precio: 300,
+    disponibles: true,
+}
+
+const medidas = {
+    peso: '1kg',
+    medida: '1m',
+}
+
+const nuevoProducto = {...producto,...medidas};
+
+console.log(producto);
+console.log(nuevoProducto);
