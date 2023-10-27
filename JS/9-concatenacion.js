f3e5aec112cd9db990b4483b38d702a4e8281216
AlejoColazurda <acolazurda@gmail.com>
2023-10-27 13:09:24 -0300

    Concatenacion y template strings en JS

---
+++ JS/9-concatenacion.js
@@ -0,0 +1,10 @@
+//CONCATENACION
+const nombre = 'Alejo',
+    email = 'acolazurda@gmail.com';
+
+//concatenacion
+console.log('Nombre Cliente: '+ nombre +' '+ "Email: " + email);
+
+//template de strings
+console.log(`Nombre Cliente: ${nombre} Email: ${email}`);
+
