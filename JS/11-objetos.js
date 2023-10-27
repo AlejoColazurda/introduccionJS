d78d076efb376c8495b0f73f091c15ddd1e2d968
AlejoColazurda <acolazurda@gmail.com>
2023-10-27 13:36:18 -0300

    Agregar y ELiminar Objetos en JS

--- JS/11-objetos.js
+++ JS/11-objetos.js
@@ -1,8 +1,8 @@
 //OBJETOS
 
-const NombreProducto = "Monitor 20 pulgadas";
-const precio = 300;
-const disponible = true;
+// const NombreProducto = "Monitor 20 pulgadas";
+// const precio = 300;
+// const disponible = true;
 
 const producto = {
     nombreProducto: "Monitor 20 pulgadas",
@@ -10,10 +10,16 @@ const producto = {
     disponibles: true,
 }
 
+// console.log(producto);
+// console.log(producto.precio);
+// console.log(producto['disponibles']);
+// console.log(`el valor del ${producto.nombreProducto} es de ${producto.precio}`);
+
+//Agregar nuevas propiedades
+producto.imagen = 'imagen.jpg';
+
+//Eliminar propiedades
+delete producto.disponibles;
+
+
 console.log(producto);
-
-console.log(producto.precio);
-
-console.log(producto['disponibles']);
-
-console.log(`el valor del ${producto.nombreProducto} es de ${producto.precio}`);
