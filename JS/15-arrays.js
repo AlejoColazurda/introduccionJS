384c6f89230795453eaa182ee971a6a5c6970ca6
AlejoColazurda <acolazurda@gmail.com>
2023-10-29 20:21:08 -0300

    Arrays en JS

---
+++ JS/15-arrays.js
@@ -0,0 +1,18 @@
+//ARREGLOS O ARRAYS
+
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(numeros);
+
+//console.table nos muestra los valores en tablas.
+console.table(numeros);
+
+//puede contener todo tipo de caracter.
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+console.table(meses);
+
+//Acceder a los valores del array
+console.log(numeros[4]);
+
+//Conocer la extension de un arreglo
+console.log(numeros.length);
+
