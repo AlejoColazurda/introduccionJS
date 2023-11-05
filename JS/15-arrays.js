e94a2dd7a89f203b17e1808c077c54f8f7b9a15f
AlejoColazurda <acolazurda@gmail.com>
2023-11-05 20:20:33 -0300

    Arrays

--- JS/15-arrays.js
+++ JS/15-arrays.js
@@ -1,18 +1,32 @@
 //ARREGLOS O ARRAYS
 
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
-console.log(numeros);
+// console.log(numeros);
 
-//console.table nos muestra los valores en tablas.
+// console.table nos muestra los valores en tablas.
+// console.table(numeros);
+
+// puede contener todo tipo de caracter.
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+// console.table(meses);
+
+// Acceder a los valores del array
+// console.log(numeros[4]);
+
+// Conocer la extension de un arreglo
+// console.log(numeros.length);
+
+numeros[5] = 60;
+
+//Con el uso de la funcion .push() vamos a agregar un valor al final del arrary. Aunque hoy en dia no recomiendan modificar los arrays.
+numeros.push(9, 10, 11);
+
+
+// la funcion .unshift() nos agrega los valores al inicio del array.
+numeros.unshift(0, -1, -2, -3);
+
 console.table(numeros);
 
-//puede contener todo tipo de caracter.
-const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
-console.table(meses);
-
-//Acceder a los valores del array
-console.log(numeros[4]);
-
-//Conocer la extension de un arreglo
-console.log(numeros.length);
-
+numeros.forEach(function(numeros) {
+    console.log(numeros);
+})
