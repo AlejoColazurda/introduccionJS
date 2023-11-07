a8714eeb3b3b5e1eef866b2d670f183d4ca5af45
AlejoColazurda <acolazurda@gmail.com>
2023-11-06 21:06:41 -0300

    funciones 2 en js

---
+++ JS/17-funciones.js
@@ -0,0 +1,21 @@
+//DECLARACION DE FUNCION
+function sumar(){
+    console.log(10*10);
+}
+
+//LLAMAR A LA FUNCION
+sumar();
+
+//EXPRECION DE LA FUNCION
+const sumar2 = function(){
+    console.log(3+3);
+}
+
+
+
+//IIFE
+(function() {
+    console.log("esto es una funcion");
+})();
+
+sumar2();
