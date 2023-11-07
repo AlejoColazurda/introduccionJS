f24c913f52f45c9ad91f35f01d4c7549079c86a4
AlejoColazurda <acolazurda@gmail.com>
2023-11-07 11:34:52 -0300

    For Loop js

---
+++ JS/25-foor-loops.js
@@ -0,0 +1,37 @@
+//For Loop
+// for(let i = 0; i < 10; i++) {
+//     if(i % 2 === 0){
+//         console.log(`El Número ${i} es par`);
+//     }else{
+//         console.log(`El Número ${i} es impar`);
+// }
+// }
+
+const carrito = [
+    {nombre: 'Monitor 20"', precio: 500},
+    {nombre: 'Television 80"', precio: 1000},
+    {nombre: 'Tablet', precio: 400},
+    {nombre: 'Auriculares', precio: 30},
+    {nombre: 'Teclado', precio: 60},
+    {nombre: 'Iphone 15', precio: 2500},
+    {nombre: 'Parlante Xiaomi', precio: 300},
+    {nombre: 'Notebook Asus', precio: 800},
+];
+
+for(let i = 0; i < carrito.length; i++){
+    console.log( carrito[i].nombre );
+}
+
+
+
+//While Loop
+
+
+
+
+
+//Do while Loop
+
+
+
+
