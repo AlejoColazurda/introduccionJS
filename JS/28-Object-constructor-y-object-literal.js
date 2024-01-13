a96c0100ecb8ef29fed2d78ec86a37ce15735228
Alejo Colazurda <acolazurda@gmail.com>
2024-01-13 18:41:27 -0300

    OB constructor y OB literal

--- JS/28-Object-constructor-y-object-literal.js
+++ JS/28-Object-constructor-y-object-literal.js
@@ -13,12 +13,25 @@ function Producto(nombre, precio, color,){
     this.color = color;
 
 }
+
+Producto.prototype.FormatearProducto = function(){
+    return `El producto ${this.nombre} tiene un precio de: $ ${this.precio}`;
+}
+
+
 const producto2 = new Producto('Monitor Curvo de 49"', 800);
 const producto3 = new Producto('LapTop', 750);
 const producto4 = new Producto('IPhone 15', 1220, 'White');
 const producto5 = new Producto('Smart Wacht', 590, 'Black');
 
+function FormatearProducto(producto){
+    return `El porducto ${producto.nombre} tiene un precio de: $ ${producto.precio}`;
+}
+
 console.log(producto2);
 console.log(producto3);
 console.log(producto4);
 console.log(producto5);
+
+console.log(FormatearProducto(producto2));
+
