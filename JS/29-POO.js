6992f09572b3ff5b71d2f6f9da8c6a76f9c91d1c
Alejo Colazurda <acolazurda@gmail.com>
2024-01-20 20:27:41 -0300

    POO en JS 2

--- JS/29-POO.js
+++ JS/29-POO.js
@@ -6,7 +6,7 @@ class Producto{
         this.precio = precio;
     }
     formatearProducto(){
-        return `El producto ${this.nombre} tiene un precio de $ ${this.precio}`;
+        return `El producto "${this.nombre}" tiene un precio de: $${this.precio}`;
     }
 }
 
@@ -16,11 +16,17 @@ const producto2 = new Producto('Laptop', 600);
 console.log(producto);
 console.log(producto2);
 
-class Libro{
-    constructor(nomlbre, precio, isbn){
-
+//Herencia de Producto
+class Libro extends Producto{
+    constructor(nombre, precio, isbn){
+/*      SUPER sirve para la herencia en POO     */
+        super(nombre, precio);
+        this.isbn = isbn;
+    }
+    formatearProducto(){
+        return `${super.formatearProducto()} y su isbn es: ${this.isbn}`;
     }
 }
 
 const libro = new Libro('Javascript la Revolucion', 270,'78950235429658');
-console.log(libro);
+console.log(libro.formatearProducto());
